bf56520c762c7edfee3fe6418bff1e1f4522165d
Varabyou Ivan <onebelbiz@gmail.com>
2023-11-24 15:35:59 +0300

    feat: add routing and detail

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HousingLocation } from "../housing-location";
+import { RouterModule } from "@angular/router";
 
 @Component({
   selector: "app-housing-location",
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
       <img
@@ -13,10 +14,13 @@ import { HousingLocation } from "../housing-location";
         alt="Exterion photo of {{ housingLocation.name }}"
         class="listing-photo"
       />
-      <h2 class="listing-header">{{ housingLocation.name }}</h2>
-      <p class="listing-header-location">
-        {{ housingLocation.city }}, {{ housingLocation.state }}
-      </p>
+      <div class="listing-description">
+        <h2 class="listing-header">{{ housingLocation.name }}</h2>
+        <p class="listing-location">
+          {{ housingLocation.city }}, {{ housingLocation.state }}
+        </p>
+        <a [routerLink]="['/details', housingLocation.id]">Learn More</a>
+      </div>
     </section>
   `,
   styleUrls: ["./housing-location.component.css"],
