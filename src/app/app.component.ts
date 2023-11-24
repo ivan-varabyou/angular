fbc35b10fabed1750c77bf90ac2a21d39293b7a4
Varabyou Ivan <onebelbiz@gmail.com>
2023-11-24 12:53:06 +0300

    feat: add home homponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   standalone: true,
   selector: "app-root",
-  template: `<h1>Hello World Yan!</h1>`,
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          arial-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home></app-home>
+      </section>
+    </main>
+  `,
   styleUrls: ["./app.component.css"],
+  imports: [HomeComponent],
 })
 export class AppComponent {
   title = "homes";
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+import { CommonModule } from "@angular/common";
+
+@Component({
+  selector: "app-home",
+  standalone: true,
+  imports: [CommonModule],
+  template: `
+    <section>
+      <form action="">
+        <input type="text" placeholder="Filter" /><button
+          class="primary"
+          type="button"
+        >
+          Search
+        </button>
+      </form>
+    </section>
+    <section class="results"></section>
+  `,
+  styleUrls: ["./home.component.css"],
+})
+export class HomeComponent {}
