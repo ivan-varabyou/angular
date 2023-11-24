03aafd72b4adcbc08e58d48a32778065bd1d188a
Varabyou Ivan <onebelbiz@gmail.com>
2023-11-24 16:05:45 +0300

    feat: add  housing services

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,19 +1,24 @@
 import { Component, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { ActivatedRoute } from "@angular/router";
+import { HousingService } from "../housing.service";
+import { HousingLocation } from "../housing-location";
 
 @Component({
   selector: "app-details",
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>details works! {{ housingLocationId }}</p> `,
+  template: ` <p>{{ housingLocation?.name }} {{ housingLocation?.id }}</p> `,
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
   router: ActivatedRoute = inject(ActivatedRoute);
-  housingLocationId = 0;
+  housingService = inject(HousingService);
+  housingLocation: HousingLocation | undefined;
 
   constructor() {
-    this.housingLocationId = Number(this.router.snapshot.params["id"]);
+    const housingLocationId = Number(this.router.snapshot.params["id"]);
+    this.housingLocation =
+      this.housingService.getHousingLocationById(housingLocationId);
   }
 }
