d165078b8d49a3e2a0fdee1fae13a69171c2fde7
Varabyou Ivan <onebelbiz@gmail.com>
2023-11-24 16:17:56 +0300

    feat: add detail page

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -8,7 +8,27 @@ import { HousingLocation } from "../housing-location";
   selector: "app-details",
   standalone: true,
   imports: [CommonModule],
-  template: ` <p>{{ housingLocation?.name }} {{ housingLocation?.id }}</p> `,
+  template: ` <article>
+    <img class="listing-photo" [src]="housingLocation?.photo" />
+    <section class="listing-description">
+      <h2 class="listing-headong">{{ housingLocation?.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation?.city }}, {{ housingLocation?.state }},
+      </p>
+    </section>
+    <section class="listing-features">
+      <h2>About this housing location</h2>
+      <ul>
+        <li>Units available: {{ housingLocation?.availableUnits }}</li>
+        <li>Does this location have wifi: {{ housingLocation?.wifi }}</li>
+        <li>Does this location have laundry: {{ housingLocation?.laundry }}</li>
+      </ul>
+    </section>
+    <section class="listing-apply">
+      <h2 class="section-heading">Apply now to live here</h2>
+      <button class="primary" type="button">Apply Now</button>
+    </section>
+  </article>`,
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
