f909322dbf639cfc943bb6ead072c6703a7a43f9
Varabyou Ivan <onebelbiz@gmail.com>
2023-12-07 12:15:06 +0300

    feat: install bootstrap

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ServerComponent implements OnInit {
 
+  serverId = 10;
+  serverStatus = false
   constructor() { }
 
-  ngOnInit(): void {
+  allowNewSe
+
+  ngOnInit() {
+  }
+
+  getServerStatus() {
+    return this.serverStatus
   }
 
 }
